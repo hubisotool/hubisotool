540f122af43d2d09bf0a660ac2fed51405822ed5
Bishaka Samuel <sbishaka@gmail.com>
2015-12-01 11:56:49 +0300

    Save event time data in iso8601 format

--- app/ctns_imprvmt/mgt_review/schedule/event/event.js
+++ app/ctns_imprvmt/mgt_review/schedule/event/event.js
@@ -2,6 +2,7 @@
  * Created by Bishaka on 20/11/2015.
  */
 var
+    Promise = require('bluebird'),
     event = angular.module('ctns_imprvmt.mgt_review.schedule.event',[])
     .config(['$stateProvider',function($stateProvider){
         $stateProvider
@@ -15,7 +16,7 @@ var
             })
 
     }])
-    .controller('ctns_imprvmt.mgt_review.schedule.eventCtrl',['$scope','_reviews_','_backend_','$stateParams','$state',function($scope,_reviews_,_backend_,$stateParams,$state){
+    .controller('ctns_imprvmt.mgt_review.schedule.eventCtrl',['$scope','_reviews_','_backend_','$stateParams','$state','_events_',function($scope,_reviews_,_backend_,$stateParams,$state,_events_){
 
         $scope.d4lts = {
             time:moment().format('LT').replace(" ","").toLowerCase(),
@@ -36,8 +37,8 @@ var
 
         $scope.addEvent = function(){
             var obj = {type:"cim.mgt_review.schedule.event",evt:jQuery.extend(true, {}, $scope.evt)};
-            obj.evt.from.date = obj.evt.from.date.toISOString();
-            obj.evt.to.date = obj.evt.to.date.toISOString();
+            obj.evt.from =  _events_.getIsoTime(obj.evt.from.date,obj.evt.from.time);
+            obj.evt.to = _events_.getIsoTime(obj.evt.to.date,obj.evt.to.time);
             _backend_.saveToDb("cim",obj).then(function(){
                 _backend_.alert({ src :"cim.mgt_review.schedule.event",title : "Event added", text :"Review has been scheduled"});
                 $state.transitionTo("ctns_imprvmt.mgt_review.schedule");
@@ -61,6 +62,18 @@ var
     }])
     .factory('_events_',[function(){
             var _gut = {};
+
+            _gut.getEvents = function(){
+            };
+
+            _gut.getIsoTime = function(date,time){
+                var t =  moment(time, ["h:mm A"]);
+                var d =  moment(date);
+                d.hour(t.hour());
+                d.minute(t.minute());
+                d.second(t.second());
+                return d.toISOString();
+            };
 
             return _gut;
     }])
