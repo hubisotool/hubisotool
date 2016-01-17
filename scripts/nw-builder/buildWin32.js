d52be071c5a453bec253db0d737f5ec352f71ef3
Bishaka Samuel <sbishaka@gmail.com>
2016-01-17 08:05:42 +0300

    Add scripts directory for advanced building

---
+++ scripts/nw-builder/buildWin32.js
@@ -0,0 +1,16 @@
+var NwBuilder = require('nw-builder');
+var nw = new NwBuilder({
+    files: ['./app/**/**','./package.json','./node_modules/**/**'],
+    platforms: ['win32'],
+    version: '0.12.2',
+    buildDir:'./build'
+});
+
+nw.on('log',  console.log);
+
+// Build returns a promise
+nw.build().then(function () {
+   console.log('all done!');
+}).catch(function (error) {
+    console.error(error);
+});
